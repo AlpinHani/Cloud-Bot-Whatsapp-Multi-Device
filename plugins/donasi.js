5b31b850bf4c144d3e18a5c437b3547be4dcff30
AlpinHani <99058202+AlpinHani@users.noreply.github.com>
2022-06-12 16:31:17 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,15 @@
 let handler =  m => m.reply(`
-╭─「 Donasi 」
-│ • ${global.donasi}
+╭─「 Donasi • Dana 」
+│ • Pulsa [082287750102]
+│ • Gopay  [082287750102]
+│ • Dana  [082287750102]
+│ • Ovo [082287750102]
+╰────
+╭─「 *NOTE* 」
+│ > Ingin donasi? Wa.me/6282287750102
+│ _Hasil donasi akan digunakan buat sewa_
+│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _24 jam tanpa kendala_
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
